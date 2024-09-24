ed7ddf7fc69bc7b01fcd134d86562176cbb0222f
arvind jadhav <126157696+arvind-ishi@users.noreply.github.com>
2024-09-24 10:54:16 +0000

    this comite

---
+++ code_02/sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace  std;
+
+int main(){
+
+int num1 ,num2,sum;
+
+cout<<"Enter the number  num1 : "<<endl;
+cin>>num1;
+cout<<"Enter the number num2 :"<<endl;
+cin>>num2;
+
+sum = num1 + num2;
+
+cout<<"sum of the two number is : "<<sum <<endl;
+return 0;
+
+
+
+}
